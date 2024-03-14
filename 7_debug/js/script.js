be4d12395a2b740368e8a92316ea49ab964d235c
celoaraujo <celoparaujo99@gmail.com>
2024-03-14 15:01:08 -0300

    depuração

--- 7_debug/js/script.js
+++ 7_debug/js/script.js
@@ -37,7 +37,7 @@ if (c == 1) {
   c = d + 2;
 }
 
-debugger;
+// debugger;
 
 for (let i = 0; i < d; i++) {
   c = c + 2;
@@ -45,7 +45,7 @@ for (let i = 0; i < d; i++) {
 
 console.log("executou o loop");
 
-debugger;
+// debugger;
 
 if (c > 5) {
   c = 25;
@@ -70,8 +70,42 @@ checkNumber({});
 checkNumber("teste");
 
 // 5- Exceptions
-let x = 10;
 
-if (x!=11){
-    throw new Error("O valor de x não pode ser diferente de 11")
+// let x = 10;
+
+// if (x!=11){
+//     throw new Error("O valor de x não pode ser diferente de 11")
+// }
+
+// 6- try catch
+try {
+  const soma = x + y;
+} catch (error) {
+  console.log(`Erro no programa: ${error}`);
 }
+
+// 7 - finally
+try {
+  const value = checkNumber("asd");
+  if (!value) {
+    throw new Error("Valores inválidos");
+  }
+} catch (error) {
+  console.log(`Opa, aconteceu um problema: ${error}`);
+}finally{
+  console.log("O código foi executado!")
+}
+
+// 8- Assertion
+
+function checkArray(arr){
+
+  if(arr.length === 0){
+    throw new Error("O array precisa ter elementos")
+  }else {
+    console.log(`O array tem ${arr.length} elementos`)
+  }
+}
+
+// checkArray([]);
+checkArray([1,2,3]);
